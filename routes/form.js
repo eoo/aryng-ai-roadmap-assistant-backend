be6d6e8e93d55cd28a32f65b6bfb00c4f34db880
Tarun Singh <tarungarhwal@gmail.com>
2025-04-25 16:42:51 +0530

    handdle errors for routes

--- routes/form.js
+++ routes/form.js
@@ -5,23 +5,28 @@ const { getDB } = require('../db/mongoClient');
 router.post('/', async (req, res) => {
   const { firstName, lastName, email, companyName, industry, companySize, jobTitle } = req.body;
 
-  if (!firstName || !lastName || !email) {
-    return res.status(400).json({ error: 'Missing required fields' });
+  try {
+    if (!firstName || !lastName || !email) {
+      return res.status(400).json({ error: 'Missing required fields' });
+    }
+
+    const db = getDB();
+    const result = await db.collection('users').insertOne({
+      first_name: firstName,
+      last_name: lastName,
+      email,
+      company_name: companyName,
+      industry,
+      company_size: companySize,
+      job_title: jobTitle,
+      created_at: new Date()
+    });
+
+    res.status(200).json({ message: 'Form submitted successfully', id: result.insertedId });
+  } catch(error) {
+    console.error('Submit Form Error:', err);
+    res.status(500).json({ error: 'Failed to submit user form.' });
   }
-
-  const db = getDB();
-  const result = await db.collection('users').insertOne({
-    first_name: firstName,
-    last_name: lastName,
-    email,
-    company_name: companyName,
-    industry,
-    company_size: companySize,
-    job_title: jobTitle,
-    created_at: new Date()
-  });
-
-  res.status(200).json({ message: 'Form submitted successfully', id: result.insertedId });
 });
 
 module.exports = router;
